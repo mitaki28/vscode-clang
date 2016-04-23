b052ac2543f3e59fa7e1aea74337363413a280fe
Eloy Durán <eloy.de.enige@gmail.com>
2016-04-23 18:34:40 +0200

    Add option to use a Clang compilation DB.

--- src/diagnostic.ts
+++ src/diagnostic.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 import * as clang from './clang';
 import * as execution from './execution';
 
-export const diagnosticRe = /^\<stdin\>:(\d+):(\d+):(?:((?:\{.+?\})+):)? ((?:fatal )?error|warning): (.*?)$/;  
+export const diagnosticRe = /^\<stdin\>:(\d+):(\d+):(?:((?:\{.+?\})+):)? ((?:fatal )?error|warning): (.*?)$/;
 function str2diagserv(str: string): vscode.DiagnosticSeverity {
     switch(str) {
         case 'fatal error': return vscode.DiagnosticSeverity.Error;
@@ -45,7 +45,7 @@ export function registerDiagnosticProvider(selector: vscode.DocumentSelector, pr
         cancellers.set(uriStr, new vscode.CancellationTokenSource);
         delay(cancellers.get(uriStr).token).then(() => {
             cancellers.get(uriStr).dispose();
-            cancellers.set(uriStr, new vscode.CancellationTokenSource);                            
+            cancellers.set(uriStr, new vscode.CancellationTokenSource);
             return provider.provideDiagnostic(change.document, cancellers.get(uriStr).token);
         }).then((diagnostics) => {
             cancellers.get(uriStr).dispose();
@@ -59,7 +59,7 @@ export function registerDiagnosticProvider(selector: vscode.DocumentSelector, pr
             for (let canceller of Array.from(cancellers.values())) {
                 canceller.dispose();
             }
-            vscode.Disposable.from(...subsctiptions).dispose();  
+            vscode.Disposable.from(...subsctiptions).dispose();
         }
     };
 }
@@ -110,15 +110,16 @@ export class ClangDiagnosticProvider implements DiagnosticProvider {
     }
 
     fetchDiagnostic(document: vscode.TextDocument, token: vscode.CancellationToken): Thenable<string> {
-        let [cmd, args] = clang.check(document.languageId);
-        return execution.processString(cmd, args, 
+        let file = document.uri.fsPath;
+        let [cmd, args] = clang.check(document.languageId, file);
+        return execution.processString(cmd, args,
             {
-                cwd: path.dirname(document.uri.fsPath),
-                maxBuffer: clang.getConf<number>('diagnostic.maxBuffer')                    
+                cwd: path.dirname(file),
+                maxBuffer: clang.getConf<number>('diagnostic.maxBuffer')
             },
             token,
             document.getText()
-        ).then((result) => result.stderr.toString());   
+        ).then((result) => result.stderr.toString());
     }
 
     parseDiagnostic(data: string): vscode.Diagnostic[] {
@@ -135,9 +136,9 @@ export class ClangDiagnosticProvider implements DiagnosticProvider {
                 let ranges = parseRanges(matched[3]);
                 range = new vscode.Range(
                     ranges[0].start.line - 1,
-                    ranges[0].start.character - 1,                    
+                    ranges[0].start.character - 1,
                     ranges[ranges.length - 1].end.line - 1,
-                    ranges[ranges.length - 1].end.character - 1                    
+                    ranges[ranges.length - 1].end.character - 1
                 );
             }
             let msg: string = matched[5];
@@ -145,6 +146,6 @@ export class ClangDiagnosticProvider implements DiagnosticProvider {
 
             result.push(new vscode.Diagnostic(range, msg, type));
         });
-        return result; 
-    }    
+        return result;
+    }
 }
