c0b97180fcfcdc6081dacf16b94bcc75922a36e6
mitaki28 <mitaki28@users.noreply.github.com>
2016-11-13 00:57:08 +0900

    more strict configuration check

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
 import * as clang from './clang'
-
+import * as execution from './execution'
 
 export class ConfigurationTester implements vscode.Disposable{
     processes: Map<number, child_process.ChildProcess>;
@@ -10,18 +10,18 @@ export class ConfigurationTester implements vscode.Disposable{
         this.processes = new Map<number, child_process.ChildProcess>();
     }
     test(language: string): void {
-        let [cmd, args] = clang.version(language);
+        let [cmd, args] = clang.check(language);
         let proc = child_process.execFile(cmd, args, (error, stdout, stderr) => {
             if (error) {
                 if ((<any>error).code == 'ENOENT') {
                     vscode.window.showErrorMessage('Please install [clang](http://clang.llvm.org/) or check configuration `clang.executable`')
                 } else {
-                    vscode.window.showErrorMessage('Please check your configurations')
+                    vscode.window.showErrorMessage('Please check your configurations: ' + stderr.toString().trim())
                 }
-                vscode.window.showErrorMessage(stderr.toString());
             }
             this.processes.delete(proc.pid);
         });
+        proc.stdin.end('int main() { return 0; }')
         this.processes.set(proc.pid, proc);
     }
     dispose() {
